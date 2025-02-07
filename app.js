ac64e8a6d37dcb9a2ef1e91d6b0c8ec33bc254dd
Mirko <mirkocaccavone03@gmail.com>
2025-02-07 16:08:25 +0100

    [feat] add body-parser middleware

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const postsRouter = require('./routers/posts');
 // Serve i file statici dalla cartella 'public'
 app.use(express.static('public'));
 
+// registro il body-parser per "application/json"
+app.use(express.json());
+
 // Gestisce la route principale ('/')
 app.get('/', (req, res) => {
     // Invia una semplice risposta testuale
